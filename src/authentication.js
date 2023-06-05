3c071a51739001328c5f0b22399f3e88f9fcc54a
chiqiman <128951818+chiqiman@users.noreply.github.com>
2023-06-05 12:56:27 +0700

    Update authentication.js

--- src/authentication.js
+++ src/authentication.js
@@ -1,114 +1,105 @@
 const accounts = require('./accounts');
 
-const Register = (request, h) => {
-  const { username, password, email } = request.payload;
-    if(!username) {
-      const response = h.response({
-          status: 'fail',
-          message: 'Gagal menambahkan username. Mohon isi username !!',
-        });
-        response.code(400);
-        return response;
-      }
-    if(!password) {
-      const response = h.response({
-          status: 'fail',
-          message: 'password salah',
-        });
-        response.code(400);
-        return response;
-      }
-    if(!email) {
-      const response = h.response({
-          status: 'fail',
-          message: 'password salah',
-        });
-        response.code(400);
-        return response;
-      }
+const Register = (req, res) => {
+  const { username, password, email } = req.body;
 
-    const id_account = nanoid(16);
-    const newAccount = {id_account, username, password, email};
-    accounts.push(newAccount);
-    
-    const isSuccess = accounts.filter((account) => account.id_account === id_account).length > 0; 
-    if (isSuccess) {
-      const response = h.response({
-        status: 'success',
-        message: 'Anda berhasil registrasi akun',
-        data: {
-          accountId: id_account
-        }
-      });
-      response.code(201);
-      return response;
-    }
+  if (!username) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Gagal menambahkan username. Mohon isi username !!',
+    });
+    return;
+  }
 
-    const response = h.response({
+  if (!password) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'password salah',
+    });
+    return;
+  }
+
+  if (!email) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'password salah',
+    });
+    return;
+  }
+
+  const id_account = nanoid(16);
+  const newAccount = { id_account, username, password, email };
+  accounts.push(newAccount);
+
+  const isSuccess = accounts.filter((account) => account.id_account === id_account).length > 0;
+  if (isSuccess) {
+    res.status(201).json({
+      status: 'success',
+      message: 'Anda berhasil registrasi akun',
+      data: {
+        accountId: id_account
+      }
+    });
+  } else {
+    res.status(500).json({
       status: 'fail',
       message: 'Gagal registrasi',
     });
-
-    response.code(500);
-    return response;
+  }
 };
 
-const Login = (request, h) => {
-  const { username, password } = request.payload; 
-  if(!username) {
-    const response = h.response({
-        status: 'fail',
-        message: 'Data yang dimasukkan salah atau tidak lengkap !!',
-      });
-      response.code(400);
-      return response;
-    }
-  if(!password) {
-    const response = h.response({
-        status: 'fail',
-        message: 'Data yang dimasukkan salah atau tidak lengkap',
-      });
-      response.code(400);
-      return response;
-    }
+const Login = (req, res) => {
+  const { username, password } = req.body;
 
-    const id_account = nanoid(16);
-    const newLogin = {username, password };
-    accounts.push(newLogin);
-    
-    const isSuccess = accounts.filter((account) => account.id_account === id_account).length > 0; 
-    if (isSuccess) {
-      const response = h.response({
-        status: 'success',
-        message: 'Anda berhasil Login',
-        data: {
-          accountId: id_account
-        }
-      });
-      response.code(201);
-      return response;
-    }
+  if (!username) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Data yang dimasukkan salah atau tidak lengkap !!',
+    });
+    return;
+  }
 
-    const response = h.response({
+  if (!password) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Data yang dimasukkan salah atau tidak lengkap',
+    });
+    return;
+  }
+
+  const id_account = nanoid(16);
+  const newLogin = { username, password };
+  accounts.push(newLogin);
+
+  const isSuccess = accounts.filter((account) => account.id_account === id_account).length > 0;
+  if (isSuccess) {
+    res.status(201).json({
+      status: 'success',
+      message: 'Anda berhasil Login',
+      data: {
+        accountId: id_account
+      }
+    });
+  } else {
+    res.status(500).json({
       status: 'fail',
       message: 'Gagal Login',
     });
-
-    response.code(500);
-    return response;
+  }
 };
 
-//logout gw liat gpt rel
-const Logout = (request, h) => {
-    req.session.destroy(err => {
-        if (err) {
-          // Handle any error that occurs during session destruction
-          console.error('Error during session destruction:', err);
-          return res.status(500).json({ error: 'Internal Server Error' });
-        }
-        // If logout is successful, send a success response
-        res.json({ message: 'Logout successful.' });
-      });
+const Logout = (req, res) => {
+    // Destroy the user's session
+    req.session.destroy((err) => {
+      // If an error occurs, log it and return a 500 error
+      if (err) {
+        console.error('Error during session destruction:', err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        // If logout is successful, redirect the user to the home page
+        res.redirect('/');
+      }
+    });
 };
 
 module.exports = {
