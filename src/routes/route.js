09e7c68b5d3b049d0717ce9f776a39e143e22dc9
airellrmdn <airell.ramadhan@ui.ac.id>
2023-06-05 15:06:32 +0700

    Minor Change

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,25 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Multer = require('multer');
 const { getPupuk, getHistory, } = require('../handlers/handler');
+const { login, register, logout } = require('../handlers/auth.handler');
+
 /*
 const {Login,Register, Logout,} = require('./authentication');
-
-app.post('/accounts', Register);
-app.post('/accounts', Login);
 app.post('/accounts', Logout);
-];
 */
-
 
 const multer = Multer({
     storage: Multer.memoryStorage,
     fileSize: 5 * 1024 * 1024
 });
 
-// router.post('/register', register);
-// router.post('/login', login);
+router.post('/register', register);
+router.post('/login', login);
 
-router.get('/listPupuk', getPupuk);
+router.get('/pupuk', getPupuk);
 router.get('/history', getHistory);
 
 // router.post('/uploadImage', multer.single('image'))
