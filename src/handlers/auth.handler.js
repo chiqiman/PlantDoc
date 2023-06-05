15736e4240ea3fef244d5f215d68b87c40c657fe
airellrmdn <airell.ramadhan@ui.ac.id>
2023-06-05 23:23:56 +0700

    Revamp Auth Handler

--- src/handlers/auth.handler.js
+++ src/handlers/auth.handler.js
@@ -1,92 +1,116 @@
-const accounts = require('../accounts');
 const connection = require('../configs/db.config');
+const bcrypt = require('bcrypt');
 
 const register = (req, res) => {
-  const { username, password, email } = req.body;
+    const { username, password, email } = req.body;
+    
+    if (!username) {
+        res.status(400).json({
+        status: 'fail',
+        message: 'Gagal menambahkan username. Mohon isi username!',
+        });
+        return;
+    }
 
-  if (!username) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Gagal menambahkan username. Mohon isi username !!',
-    });
-    return;
-  }
+    if (!password) {
+        res.status(400).json({
+        status: 'fail',
+        message: 'Gagal menambahkan password. Mohon isi password!',
+        });
+        return;
+    }
 
-  if (!password) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'password salah',
-    });
-    return;
-  }
+    if (!email) {
+        res.status(400).json({
+        status: 'fail',
+        message: 'Gagal menambahkan email. Mohon isi email!',
+        });
+        return;
+    }
 
-  if (!email) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'password salah',
-    });
-    return;
-  }
+    const checkQuery = "SELECT * FROM account WHERE LOWER(email) = LOWER(?)";
+    const id_account = nanoid(16);
 
-  const id_account = nanoid(16);
-  const newAccount = { id_account, username, password, email };
-  accounts.push(newAccount);
-
-  const isSuccess = accounts.filter((account) => account.id_account === id_account).length > 0;
-  if (isSuccess) {
-    res.status(201).json({
-      status: 'success',
-      message: 'Anda berhasil registrasi akun',
-      data: {
-        accountId: id_account
-      }
+    connection.query(checkQuery, [email], (err, rows) => {
+        if (rows.length) {
+            return res.status(409).send({message: "Akun ini sudah ada!"});
+        } else {
+            bcrypt.hash(password, 10, (err, hash) => {
+                if (err) {
+                    return res.status(500).send({message: err});
+                } else {
+                    const createQuery = `INSERT INTO account (id_account, username, password, email) VALUES ('${id_account}', '${username}', '${hash}', '${email}')`;
+                    connection.query(createQuery, (err, rows) => {
+                        if (err) {
+                            return res.status(400).send({message: err});
+                        }
+                        console.log(rows);
+                        res.status(201).send({message: "Register Success"});
+                    });
+                }
+            });
+        }
     });
-  } else {
-    res.status(500).json({
-      status: 'fail',
-      message: 'Gagal registrasi',
-    });
-  }
 };
 
 const login = (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  if (!username) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Data yang dimasukkan salah atau tidak lengkap !!',
-    });
-    return;
-  }
+    if (!username) {
+        res.status(400).json({
+        status: 'fail',
+        message: 'Data yang dimasukkan salah atau tidak lengkap !!',
+        });
+        return;
+    }
 
-  if (!password) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Data yang dimasukkan salah atau tidak lengkap',
-    });
-    return;
-  }
+    if (!password) {
+        res.status(400).json({
+        status: 'fail',
+        message: 'Data yang dimasukkan salah atau tidak lengkap',
+        });
+        return;
+    }
 
-  const id_account = nanoid(16);
-  const newLogin = { username, password };
-  accounts.push(newLogin);
+    const checkQuery = "SELECT * FROM account WHERE username = ?";
+    connection.query(checkQuery, [username], (err, rows) => {
+        if (err) {
+            return res.status(400).send({message: err});
+        }
+        bcrypt.compare(password, rows[0].password, (err, result) => {
+            if (err) {
+                console.log('Username atau Password tidak ditemukan!');
+                return res.status(401).send({message: 'Email atau Password salah'});
+            }
+            if (result) {
+                res.send({
+                    message: 'Login Success',
+                    user: rows[0],
+                });
+            }
+            return res.status(401).send({message: 'Email atau Password salah'});
+        });
+    });
 
-  const isSuccess = accounts.filter((account) => account.id_account === id_account).length > 0;
-  if (isSuccess) {
-    res.status(201).json({
-      status: 'success',
-      message: 'Anda berhasil Login',
-      data: {
-        accountId: id_account
-      }
-    });
-  } else {
-    res.status(500).json({
-      status: 'fail',
-      message: 'Gagal Login',
-    });
-  }
+    // const id_account = nanoid(16);
+    // const newLogin = { username, password };
+    // accounts.push(newLogin);
+
+    // const isSuccess = accounts.filter((account) => account.id_account === id_account).length > 0;
+    // if (isSuccess) {
+    //     res.status(201).json({
+    //     status: 'success',
+    //     message: 'Anda berhasil Login',
+    //     data: {
+    //         accountId: id_account
+    //     }
+    //     });
+    // } else {
+    //     res.status(500).json({
+    //     status: 'fail',
+    //     message: 'Gagal Login',
+    //     });
+    // }
 };
 
 const logout = (req, res) => {
